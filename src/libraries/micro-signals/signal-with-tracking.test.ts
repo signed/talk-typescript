1b96df153b3a9218df658f2c31e58f19bce81f28
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2021-02-13 16:37:39 +0100

    a signal with tracking

---
+++ src/libraries/micro-signals/signal-with-tracking.test.ts
@@ -0,0 +1,30 @@
+import { ReadableSignal, Signal } from 'micro-signals';
+import { SignalWithTracking, TrackingContext } from './signal-with-tracking';
+
+test('should ', () => {
+    const signal = new Signal<string>();
+
+    const trackingContext: TrackingContext<string> = {
+        signal,
+        signalsWithSubscription: new Set<ReadableSignal<unknown>>(),
+        tag: { marker: 'one' }
+    };
+    const trackedSignal: ReadableSignal<string> = new SignalWithTracking(trackingContext);
+
+    const one = () => {
+        throw new Error('evil');
+    };
+    const two = () => {
+        throw new Error('evil');
+    };
+
+    trackedSignal.addOnce(one);
+    trackedSignal.addOnce(two);
+    expect(trackingContext.signalsWithSubscription.size).toBe(1);
+
+    trackingContext.signalsWithSubscription.forEach(signal => signal.remove(trackingContext.tag));
+
+    expect(trackingContext.signalsWithSubscription.size).toBe(0);
+
+    signal.dispatch('hello');
+});
---
+++ src/libraries/micro-signals/signal-with-tracking.ts
@@ -0,0 +1,97 @@
+import { ReadableSignal, Listener, Cache, Accumulator } from 'micro-signals';
+
+export interface TrackingContext<T> {
+    tag: Object;
+    //should ths be a WeakReference?
+    signal: ReadableSignal<T>
+    // should this be a WeakSet?
+    signalsWithSubscription: Set<ReadableSignal<unknown>>
+}
+
+export const trackingContext = <T>(previous: TrackingContext<unknown>, signal: ReadableSignal<T>): TrackingContext<T> => {
+    return {
+        tag: previous.tag,
+        signalsWithSubscription: previous.signalsWithSubscription,
+        signal
+    };
+};
+
+const signalWithTracking = <T>(previous: TrackingContext<unknown>, signal: ReadableSignal<T>) => {
+    const updateContext = trackingContext(previous, signal);
+    return new SignalWithTracking(updateContext);
+};
+
+export class SignalWithTracking<T> implements ReadableSignal<T> {
+
+    constructor(private readonly trackingContext: TrackingContext<T>) {
+    }
+
+    add(listener: Listener<T>, ...tags: any[]): void {
+        if (tags === undefined) {
+            throw new Error('lets check this');
+        }
+        const signal = this.signal();
+        signal.add(listener, this.trackingContext.tag);
+        this.trackingContext.signalsWithSubscription.add(this);
+    }
+
+    addOnce(listener: Listener<T>, ...tags: any[]): void {
+        if (tags === undefined) {
+            throw new Error('lets check this');
+        }
+        const signal = this.signal();
+        signal.addOnce(listener, this.trackingContext.tag);
+        this.trackingContext.signalsWithSubscription.add(this);
+    }
+
+    remove(listenerOrTag: any): void {
+        if (this.trackingContext.tag === listenerOrTag) {
+            this.trackingContext.signalsWithSubscription.delete(this);
+        }
+        this.signal().remove(listenerOrTag);
+    }
+
+    cache(cache: Cache<T>): ReadableSignal<T> {
+        const cachedSignal = this.trackingContext.signal.cache(cache);
+        return signalWithTracking(this.trackingContext, cachedSignal);
+    }
+
+    filter<U extends T>(filter: (payload: T) => payload is U): ReadableSignal<U>;
+    filter(filter: (payload: T) => boolean): ReadableSignal<T>;
+    filter(filter: any): any {
+        const filtered = this.signal().filter(filter);
+        return signalWithTracking(this.trackingContext, filtered);
+    }
+
+    map<U>(transform: (payload: T) => U): ReadableSignal<U> {
+        const mappedSignal = this.trackingContext.signal.map(transform);
+        return signalWithTracking(this.trackingContext, mappedSignal);
+    }
+
+    merge<U>(...signals: ReadableSignal<U>[]): ReadableSignal<T | U> {
+        const merged = this.trackingContext.signal.merge(...signals);
+        return signalWithTracking(this.trackingContext, merged);
+    }
+
+    peek(peekaboo: (payload: T) => void): ReadableSignal<T> {
+        this.signal().peek(peekaboo);
+        return this;
+    }
+
+    promisify(rejectSignal?: ReadableSignal<any>): Promise<T> {
+        return this.signal().promisify(rejectSignal);
+    }
+
+    readOnly(): ReadableSignal<T> {
+        return this;
+    }
+
+    reduce<U>(accumulator: Accumulator<T, U>, initialValue: U): ReadableSignal<U> {
+        const reducedSignal = this.signal().reduce(accumulator, initialValue);
+        return signalWithTracking(this.trackingContext, reducedSignal);
+    }
+
+    private signal() {
+        return this.trackingContext.signal;
+    }
+}
