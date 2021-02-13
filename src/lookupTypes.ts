907ce5d2ebb73187d2b7520aa4ab6426b3d2639e
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2021-02-13 14:52:06 +0100

    extend lookup types example

--- src/lookupTypes.ts
+++ src/lookupTypes.ts
@@ -2,27 +2,28 @@
 // https://dev.to/busypeoples/notes-on-typescript-mapped-types-and-lookup-types-i36
 
 
-interface SomeInterface{
+interface SomeInterface {
     justAProperty: Date;
     anotherOne: string;
+    finalOne: number
 }
 
 const instance: SomeInterface = {
-    anotherOne: 'flupp', justAProperty: new Date()
+    anotherOne: 'flupp', justAProperty: new Date(), finalOne: 42
+};
 
-}
-
-type TypeOfJustAProperty = SomeInterface['justAProperty'];
+type SingleProperty = SomeInterface['justAProperty'];
+type MultipleProperties = SomeInterface['justAProperty' | 'anotherOne']
 type AllPropertiesOfSomeInterface = keyof SomeInterface;
 
-const accessProperty = ( obj: SomeInterface, key: AllPropertiesOfSomeInterface) => {
-    return obj[key]
-}
+const accessProperty = (obj: SomeInterface, key: AllPropertiesOfSomeInterface) => {
+    return obj[key];
+};
 
-const _first = accessProperty(instance, 'anotherOne' )
+const _first = accessProperty(instance, 'anotherOne');
 
 function getProperty<T, K extends keyof T>(obj: T, key: K) {
     return obj[key]; // Inferred type is T[K]
 }
 
-const _second = getProperty(instance, 'justAProperty')
+const _second = getProperty(instance, 'justAProperty');
