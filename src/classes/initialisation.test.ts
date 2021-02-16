de8e93925521832ec5ee2f47d8b1dc005dbbf8ea
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2021-02-16 23:53:56 +0100

    member initialisation in classes

---
+++ src/classes/initialisation.test.ts
@@ -0,0 +1,23 @@
+export class AssignedInParameter {
+    // derived will be undefined, is this a bug in typescript?
+    readonly derived: string = this.parameter;
+
+    constructor(public readonly parameter: string) {
+    }
+}
+
+test('assignment in parameter is not available when initializing other fields   ', () => {
+    expect(new AssignedInParameter('value').derived).toEqual(undefined);
+});
+
+export class AssignedInConstructorBody {
+    readonly derived: string;
+
+    constructor(public readonly parameter: string) {
+        this.derived = this.parameter;
+    }
+}
+
+test('assignment in parameter is not available when initializing other fields   ', () => {
+    expect(new AssignedInConstructorBody('value').derived).toEqual('value');
+});
