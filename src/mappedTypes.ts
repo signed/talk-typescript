261123ad8d67b416241661eca7d386aadde369e7
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2021-03-03 22:05:05 +0100

    bit of meta programming

--- src/mappedTypes.ts
+++ src/mappedTypes.ts
@@ -1,2 +1,43 @@
 // https://mariusschulz.com/blog/typescript-2-1-mapped-types
 // https://github.com/Microsoft/TypeScript/blob/0a535f0bf7193741e6b4acf5b7dfea88e2d4beca/lib/lib.d.ts#L1379-L1405
+// https://www.dslemay.com/blog/2020/05/25/typescript-utility-types-part-3-extract-exclude-and-nonnullable
+
+
+/**
+ * keyof
+ *
+ * It will just expose the keys of any given interface/type through a union
+ */
+/**
+ * infer
+ * https://www.typescriptlang.org/docs/handbook/advanced-types.html#type-inference-in-conditional-types
+ */
+
+type KeyOfAny = keyof any  // string | number | symbol
+type KeyOfUnknown = keyof object
+
+// each type in T is checked against the constraint separately
+type Distributive<T> = T extends string | number ? T : never
+type ApplyOne = Distributive<string>
+type ApplyTwo = Distributive<number>
+type ApplyThree = Distributive<string | number>
+type ApplyFour = Distributive<string | number | boolean>
+
+// the [] around T force a check of the entire type against the extends constraint of T
+type NoneDistributive<T> = [T] extends [string | number] ? T : never
+type NoneApplyOne = NoneDistributive<string>
+type NoneApplyTwo = NoneDistributive<number>
+type NoneApplyThree = NoneDistributive<string | number>
+type NoneApplyFour = NoneDistributive<string | number | boolean>
+
+
+// check if an empty object was passed
+type Empty<T extends {}> = {} extends Required<T> ? true : false;
+type isEmpty = Empty<{}>; // true
+type isNotEmpty = Empty<{ name: string }>; // false
+
+// length of a tuple
+type Bang<T extends [...any]> = T['length'] extends 1 ? true : void;
+type TooShort = Bang<[]>
+type CorrectLength = Bang<[string]>
+type TooLong = Bang<[string]>
