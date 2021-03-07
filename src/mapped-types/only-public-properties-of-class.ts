581210d12565c7c6766f8744bbc58ee178c94120
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2021-03-07 23:11:36 +0100

    another interesting type example

---
+++ src/mapped-types/only-public-properties-of-class.ts
@@ -0,0 +1,19 @@
+// https://stackoverflow.com/a/60390007
+
+type PublicInterface<T> = Pick<T, keyof T>;
+
+class DefaultBuilding {
+    constructor(private constructionYear: number, public name: string) {
+    }
+
+    public color() {
+        return 'blue';
+    }
+
+    private security() {
+
+    }
+}
+
+
+type Building = PublicInterface<DefaultBuilding>
