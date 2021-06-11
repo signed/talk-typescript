b506379c72d662c58a314d1ed6a9a0a64407bc29
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2021-06-11 14:22:33 +0200

    template literal types

--- src/stringLiteralTypes.ts
+++ src/stringLiteralTypes.ts
@@ -13,3 +13,18 @@ current = 'yellow';
 /* A string literal type can be considered a subtype of the string type.
  This means that a string literal type is assignable to a plain string, but not vice-versa.
  */
+
+// Template literal types
+//https://ikoshelev.azurewebsites.net/search/id/16/Pragmatic-uses-of-TypeScript-type-system-03-Tag-hierarchies-via-Template-Literal-Types?utm_source=typescript-weekly.com&utm_campaign=typescript_weekly_171&utm_medium=email
+
+type VanTag = `van${string}`;
+const vanTag: VanTag = `van`;
+
+type AmbulanceTag = `van;ambulance${string}`
+const ambulanceTag: AmbulanceTag = 'van;ambulance' as const
+
+const hello = (_vanTag: VanTag) => true;
+
+hello(vanTag)
+hello(ambulanceTag)
+
