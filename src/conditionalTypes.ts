19c6d4564cfd71f529d9621ef778ef97bde4ceda
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2021-08-28 18:18:56 +0200

    conditional types example

--- src/conditionalTypes.ts
+++ src/conditionalTypes.ts
@@ -1,5 +1,11 @@
 // https://codewithstyle.info/TypeScript-conditional-types-real-life-example/
-
 type IsString<T> = T extends string ? 'yes' : 'no';
 
 const nope: IsString<'banana'> = 'yes';
+
+
+// https://rossbulat.medium.com/typescript-conditionals-explained-a096591f3ac0
+// filter out all other types from the union besides string or number
+type StringOrNumberOnly<T> = T extends string | number ? T : never;
+type MyResult = StringOrNumberOnly<string | number | boolean>;
+// MyResult = string | number
