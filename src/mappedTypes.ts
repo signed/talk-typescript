685c739a2a365c678d73e30b603b4ecbdfba59b6
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2021-08-28 10:25:03 +0200

    mapped types

--- src/mappedTypes.ts
+++ src/mappedTypes.ts
@@ -2,19 +2,35 @@
 // https://github.com/Microsoft/TypeScript/blob/0a535f0bf7193741e6b4acf5b7dfea88e2d4beca/lib/lib.d.ts#L1379-L1405
 // https://www.dslemay.com/blog/2020/05/25/typescript-utility-types-part-3-extract-exclude-and-nonnullable
 
-
 /**
  * keyof
  *
  * It will just expose the keys of any given interface/type through a union
  */
+type KeyOfAny = keyof any  // string | number | symbol
+type KeyOfUnknown = keyof object
+
+/**
+ * typeof
+ * extract type from an object
+ */
+const someObj = { foo: 42, bar: 'Hello' };
+type SomeObjType = typeof someObj
+
+/**
+ * extract subtypes from a type
+ */
+type WithSubTypes = {
+    sub : {one:string}[],
+    marine: number
+}
+type Extracted = WithSubTypes['sub']
+
 /**
  * infer
  * https://www.typescriptlang.org/docs/handbook/advanced-types.html#type-inference-in-conditional-types
  */
-
-type KeyOfAny = keyof any  // string | number | symbol
-type KeyOfUnknown = keyof object
+// TODO https://files.peterkroener.de/slides/ts60/#45/2
 
 // each type in T is checked against the constraint separately
 type Distributive<T> = T extends string | number ? T : never
