9385e71ca671ed91d61247d54ba5e075ce41e3f6
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2021-10-06 19:37:13 +0200

    add link an format

--- src/enumerations.ts
+++ src/enumerations.ts
@@ -1,63 +1,64 @@
 // https://www.typescriptlang.org/docs/handbook/enums.html
+// https://robinpokorny.com/blog/typescript-enums-i-want-to-actually-use (why you should use const enums)
 
 enum StringEnumeration {
-    One = 'One',
-    Two = 'Two',
-    Three = 'Three',
+  One = 'One',
+  Two = 'Two',
+  Three = 'Three',
 }
 
 namespace StringEnumeration {
-    export function valuesAsArray(): StringEnumeration[] {
-        return [StringEnumeration.One, StringEnumeration.Two, StringEnumeration.Three];
-    }
+  export function valuesAsArray(): StringEnumeration[] {
+    return [StringEnumeration.One, StringEnumeration.Two, StringEnumeration.Three]
+  }
 }
 
-type StringEnumerationStrings = Exclude<keyof typeof StringEnumeration, 'valuesAsArray' >;
+type StringEnumerationStrings = Exclude<keyof typeof StringEnumeration, 'valuesAsArray'>
 
 {
-    const assertUnreachable = (arg: never): never => {
-        throw new Error('Did not expect this be executed ever');
-    };
+  const assertUnreachable = (_arg: never): never => {
+    throw new Error('Did not expect this be executed ever')
+  }
 
-    const exhaustiveSwitch = (value: StringEnumeration) => {
-        switch (value) {
-            case StringEnumeration.One:
-                return 1;
-            case StringEnumeration.Two:
-                return 2;
-            case StringEnumeration.Three:
-                return 3;
-        }
-        assertUnreachable(value);
-    };
+  const exhaustiveSwitch = (value: StringEnumeration) => {
+    switch (value) {
+      case StringEnumeration.One:
+        return 1
+      case StringEnumeration.Two:
+        return 2
+      case StringEnumeration.Three:
+        return 3
+    }
+    assertUnreachable(value)
+  }
 }
 
-console.log();
-console.log('const value in StringEnumeration.valuesAsArray()');
+console.log()
+console.log('const value in StringEnumeration.valuesAsArray()')
 for (const value in StringEnumeration.valuesAsArray()) {
-    console.log(value);
+  console.log(value)
 }
 
-console.log();
-console.log('const value of StringEnumeration.valuesAsArray()');
+console.log()
+console.log('const value of StringEnumeration.valuesAsArray()')
 for (const value of StringEnumeration.valuesAsArray()) {
-    console.log(value);
+  console.log(value)
 }
 
-console.log();
-console.log('const value in StringEnumeration');
+console.log()
+console.log('const value in StringEnumeration')
 for (const value in StringEnumeration) {
-    console.log(value);
+  console.log(value)
 }
 
-console.log();
-console.log('const value in Object.values(StringEnumeration)');
+console.log()
+console.log('const value in Object.values(StringEnumeration)')
 for (const value in Object.values(StringEnumeration)) {
-    console.log(value);
+  console.log(value)
 }
 
-console.log();
-console.log('const value in Object.keys(StringEnumeration)');
+console.log()
+console.log('const value in Object.keys(StringEnumeration)')
 for (const value in Object.keys(StringEnumeration)) {
-    console.log(value);
+  console.log(value)
 }
