2860c89f652962c4a37944637bab2edec11ac594
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2021-10-16 21:22:02 +0200

    convert to module/format

--- src/interfaces/incomplete-implements.test.ts
+++ src/interfaces/incomplete-implements.test.ts
@@ -1,28 +1,28 @@
-
+export {}
 
 interface WithReadWriteProperty {
-    property: string
+  property: string
 }
 
 // https://github.com/microsoft/TypeScript/issues/11596#issuecomment-256658202
 // IMO this should be a type error
 class ReadOnlyImplementation implements WithReadWriteProperty {
-    get property() {
-        return 'one'
-    }
+  get property() {
+    return 'one'
+  }
 }
 
 // https://github.com/microsoft/TypeScript/issues/33621
 class WriteOnlyImplementation implements WithReadWriteProperty {
-    set property(value:string) {
-        console.log(value)
-    }
+  set property(value: string) {
+    console.log(value)
+  }
 }
 
 test('this came as a surprise to me ', () => {
-    const writeOnlyInstance: WithReadWriteProperty = new WriteOnlyImplementation()
-    expect(writeOnlyInstance.property).toBe(undefined)
+  const writeOnlyInstance: WithReadWriteProperty = new WriteOnlyImplementation()
+  expect(writeOnlyInstance.property).toBe(undefined)
 
-    const readOnlyInstance: WithReadWriteProperty = new ReadOnlyImplementation()
-    expect(()=> readOnlyInstance.property = 'wohoh').toThrow(TypeError)
-});
+  const readOnlyInstance: WithReadWriteProperty = new ReadOnlyImplementation()
+  expect(() => (readOnlyInstance.property = 'wohoh')).toThrow(TypeError)
+})
