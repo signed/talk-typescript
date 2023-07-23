4f3b49e901bf8949228c293820a7b30eaab8e659
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2023-07-23 23:29:13 +0200

    backup previous changes

---
+++ src/brands-for-nominal-typing.ts
@@ -0,0 +1,2 @@
+// effective typescript Item 37: Consider “Brands” for Nominal Typing
+// https://github.com/danvk/effective-typescript/blob/master/samples/ch04-design/item-37-brands/brands-08.ts
---
+++ src/coding-in-the-typesystem.ts
@@ -0,0 +1,16 @@
+export {}
+// https://www.typescriptlang.org/docs/handbook/2/types-from-types.html
+
+// https://stackoverflow.com/questions/51528780/typescript-check-typeof-against-custom-type/51529486#comment105389691_51529486
+// explains
+const values = ['tell', 'me', 'why'] as const
+type Step = typeof values
+type ByHand = typeof values[0] | typeof values[1] | typeof values[2]
+type TypeOfAllKeysAccessibleUsingANumberIndex = Step[number]
+
+// return typs based on input parameters
+//https://stackoverflow.com/questions/54165536/typescript-function-return-type-based-on-input-parameter
+
+// type to remove trailing spaces in a string
+// handle tail recursive types in the typesystem
+// https://devblogs.microsoft.com/typescript/announcing-typescript-4-5-beta/#tailrec-conditional
