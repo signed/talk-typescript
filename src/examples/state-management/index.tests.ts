564e45d04ba5d775a5f8327ddf905a330b34ae1f
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2023-08-26 21:42:08 +0200

    expected to be unused

--- src/examples/state-management/index.tests.ts
+++ src/examples/state-management/index.tests.ts
@@ -3,7 +3,7 @@ import { Store } from './index'
 
 test('basic action', () => {
   const store = new Store({ value: 'initial' })
-  const action = store.action((state: State, n: number, k: string, d: Date) => {
+  const action = store.action((_state: State, n: number, _k: string, _d: Date) => {
     return { value: `updated ${n}` }
   })
   action(45, '45', new Date())
