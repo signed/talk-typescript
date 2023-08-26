4cc58c39c9ef954ae8fca4edf070a22ca8c4a321
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2023-08-26 22:22:01 +0200

    first type tests

--- src/examples/state-management/index.tests.ts
+++ src/examples/state-management/index.tests.ts
@@ -1,12 +1,18 @@
-import { test, expect } from 'vitest'
+import { test, expect, assertType, expectTypeOf } from 'vitest'
 import { Store } from './index'
 
 test('basic action', () => {
   const store = new Store({ value: 'initial' })
+
   const action = store.action((_state: State, n: number, _k: string, _d: Date) => {
     return { value: `updated ${n}` }
   })
-  action(45, '45', new Date())
+
+  expectTypeOf(action).parameter(0).toMatchTypeOf<number>()
+  expectTypeOf(action).parameter(1).toMatchTypeOf<string>()
+  expectTypeOf(action).parameter(2).toMatchTypeOf<Date>()
+  expectTypeOf(action).parameter(3).toMatchTypeOf<unknown>()
+  assertType<(n: number, k: string, d: Date) => void>(action)
 
   expect(store.state.value).toEqual('updated 45')
 })
--- testing-types.md
+++ testing-types.md
@@ -1,2 +1,5 @@
 https://www.totaltypescript.com/how-to-test-your-types
 https://2ality.com/2022/11/testing-static-types-typescript.html
+
+https://vitest.dev/guide/testing-types.html
+https://github.com/mmkal/expect-type
